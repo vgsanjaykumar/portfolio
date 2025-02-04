3d5dbbb06d97ce8651f0e1adb29cdeb734bb90ea
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-02-04 14:49:05 +0530

    m

--- src/com/Project.jsx
+++ src/com/Project.jsx
@@ -7,28 +7,28 @@ import ecomm from '/asset/e-com.jpg'
 const Project = () => {
   return (
       <section className='flex  proj-sec flex-col justify-center   ' >
-        <div className='w-full   ' id='Project'>
+        <div className='   ' id='Project'>
          <div className='  proj-sec1 w-full '>
           <h1 className='text-3xl font-bold proj-head'>Projects</h1>
          </div>
             </div>
        
-        <div className='w-full '>
-          <div className='flex flex-col  md:flex-row proj-img'>
-            <div className="relative">
-               <img className='h-[250px] w-[500px]' src={landingpage} alt="" />
+        <div className='w-full  '>
+          <div className='flex flex-col  md:flex-row proj-img '>
+            <div className="relative ">
+               <img className='h-[300px] w-[400px] md:h-[400px] md:w-48' src={landingpage} alt="" />
                <div className="proj-desc">
                 <p className='text-center py-5 proj-titlr '>landingpage using ereact js hjbfjhadhailsjdnhaisludn</p>
                </div>
             </div>
             <div className="relative">
-             <img className='h-[250px] w-[500px]' src={ecomm} alt="" />
+             <img className='h-[300px] w-[400px] md:h-[400px] md:w-48' src={ecomm} alt="" />
              <div className="proj-desc">
                 <p className='proj-titlr'>landingpage using ereact js hjbfjhadhailsjdnhaisludn</p>
                </div>
             </div>
             <div className="relative">
-              <img className='h-[250px] w-[500px]' src={landingpage2} alt="" />
+              <img className='h-[300px] w-[400px] md:h-[400px] md:w-48' src={landingpage2} alt="" />
               <div className="proj-desc">
                 <p className='proj-titlr'>landingpage using ereact js hjbfjhadhailsjdnhaisludn</p>
                </div>
@@ -38,7 +38,7 @@ const Project = () => {
        
         </div>
 
-        <div className='w-full sec-col '>
+        {/* <div className='w-full sec-col '>
           <div className='flex  proj-img flex-col  md:flex-row'>
             <div className="relative">
                <img className='h-[250px] w-[500px]' src={landingpage} alt="" />
@@ -83,7 +83,7 @@ const Project = () => {
     
             </div>
           </div>
-          </div>
+          </div> */}
       </section>
   )
 }
