26392c5a6ab5364ae91e8cd9f56cce3a88eb5543
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-02-09 13:42:50 +0530

    sadca

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import './App.css'
 import Headers from './com/Header'
 import Hero from './com/Hero'
 import Aboutme from './com/Aboutme'
-
 import Resume from './com/Resume'
 import Contact from './com/Contact'
 import Footer from './com/Footer'
-import Experience from './com/Experience'
 import Projectslide from './com/Projectslide'
 import Skill from './com/Skills'
 import Exp from './com/Exp'
@@ -16,7 +14,7 @@ import Exp from './com/Exp'
 
 const App = () => {
   return (
-    <div>
+    <div >
     <Headers/>
     <Hero/>
     <Aboutme/>
